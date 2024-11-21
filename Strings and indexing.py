7ae2dbf40a639b835fbc99909181ae40f61390c6
DaniilMalyugin <malyugin.daniil2016@gmail.com>
2024-11-22 00:28:11 +0500

    Strings and indexing.py

---
+++ Strings and indexing.py
@@ -0,0 +1,11 @@
+example = ('Escape from  Tarkov')
+#первый символ этой строки.
+print(example[0])
+#последний символ этой строки
+print(example[-1])
+#вторую половину этой строки
+print(example[9:])
+#это слово наоборот
+print(example[::-1])
+#каждый второй символ этой строки
+print(example[0::2])

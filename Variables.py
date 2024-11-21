9fc4ca53d33e54aff6317727059b11c5037f120a
DaniilMalyugin <malyugin.daniil2016@gmail.com>
2024-11-22 00:04:31 +0500

    Variables.py

---
+++ Variables.py
@@ -0,0 +1,7 @@
+name = 'Курс: Python,'
+
+DZ_count = 12
+HR_count = 1.5
+MD_count = 0.125
+
+print(f"{name} всего задач {DZ_count}, затрачено часов {HR_count}, среднее время выполнения {MD_count} часа ")

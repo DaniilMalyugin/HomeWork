887b83df0c012e3ea820a12a5cf935b8f25e42b9
DaniilMalyugin <malyugin.daniil2016@gmail.com>
2024-11-22 01:32:03 +0500

    module_1_4.py

---
+++ module_1_4.py
@@ -0,0 +1,14 @@
+my_string = ('Я продаю людей и бананы')
+#Вывести количество символов введённого текста
+print(len(my_string))
+#Выведите строку my_string в верхнем регистре.
+print(my_string.upper())
+#Выведите строку my_string в нижнем регистре.
+print(my_string.lower())
+#Измените строку my_string, удалив все пробелы
+my_string = my_string.replace(' ', '')
+print(my_string)
+#Выведите первый символ строки my_string
+print(my_string[0])
+#Выведите последний символ строки my_string
+print(my_string[-1])
